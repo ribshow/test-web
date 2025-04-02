1e37dac07146ecfe75e47abb8c018b684bb2bcd2
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2025-04-02 17:13:39 -0300

    fix(vite)

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
